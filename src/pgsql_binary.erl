c03f12d2626488c83ad1e7060dccf91910e8ce56
Leonard Boyce <leonard@verias.com>
2025-07-25 16:19:38 -0400

    (fix) lists of maps are legal...

--- src/pgsql_binary.erl
+++ src/pgsql_binary.erl
@@ -42,6 +42,7 @@ encode(cidr, B)                             -> encode(bytea, encode_net(B));
 encode(json, B) when is_binary(B)           -> <<(byte_size(B)):?int32, B/binary>>;
 encode(json, B) when is_map(B)              -> encode(json, json_encode(B));
 encode(json, B) when is_list(B)             -> encode(json, json_encode(B));
+encode(jsonb, B) when is_list(B)            -> encode(jsonb, json_encode(B));
 encode(jsonb, B) when is_map(B)             -> encode(jsonb, json_encode(B));
 encode(jsonb, B) when is_binary(B)          -> <<(byte_size(B) + 1):?int32, ?JSONB_VER:8, B/binary>>;
 encode(boolarray, L) when is_list(L)        -> encode_array(bool, L);
